5f95cf691103234efe88563ed4ae68f6388d451e
bookwed <bookwed@163.com>
2020-10-18 22:17:55 +0800

    完成使用RestTemplate来进行服务间的调用

--- springcloud-study-goods-service/src/main/java/com/wei/controller/GoodsController.java
+++ springcloud-study-goods-service/src/main/java/com/wei/controller/GoodsController.java
@@ -1,16 +1,34 @@
 package com.wei.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RequestMapping("goods")
 @RestController
 public class GoodsController {
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
 
     @GetMapping("get")
-    public String get(){
-        return "这里需要调用用户服务";
+    public String get(String name){
+        //使用RestTemplate来调用UserService
+        List<ServiceInstance> instanceList = discoveryClient.getInstances("user-service");
+        ServiceInstance instance = instanceList.get(0);
+        String host = instance.getHost();
+        int port= instance.getPort();
+        String s = restTemplate.getForObject("http://" + host + ":" + port + "/user/hello?name={1}", String.class, name);
+        return "这里是调用user-service返回的结果：" + s;
     }
 }
