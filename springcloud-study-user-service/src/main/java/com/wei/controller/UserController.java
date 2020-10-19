a946efa0489cc9703780f27a169fa37806641a0a
bookwed <bookwed@163.com>
2020-10-19 22:15:13 +0800

    手动实现轮询负债均衡

--- springcloud-study-user-service/src/main/java/com/wei/controller/UserController.java
+++ springcloud-study-user-service/src/main/java/com/wei/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.wei.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    @Value("${server.port}")
+    Integer port;
+
     @GetMapping("hello")
     public String hello(String name){
         return "hello " + name;
+    }
+
+    @GetMapping("balance")
+    public String balance(String name){
+        System.out.println(port);
+        return "hello " + name + "："+ port;
     }
 
 }
